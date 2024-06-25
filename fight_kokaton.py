c48ebfd8fe8d7a55bc951953804de7018e24da02
yoshiki <c0b23050fd@edu.teu.ac.jp>
2024-06-25 16:37:39 +0900

    演習１ スコア表示

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -141,6 +141,22 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Score:
+    """
+    爆弾の破壊数に応じてスコアを出す
+    """
+    def __init__(self):
+        self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.num = 0
+        self.img = self.fonto.render(f"スコア:{self.num}", 0, (0,0,255))
+        self.rct = self.img.get_rect()
+        self.rct.center = 100, HEIGHT-50
+    
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render(f"スコア:{self.num}", 0, (0,0,255))
+        screen.blit(self.img, self.rct)
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -149,6 +165,7 @@ def main():
     beam = None
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)] #"_"で、iを使わずに行ける
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -182,6 +199,7 @@ def main():
                 if bombs[i].rct.colliderect(beam.rct):
                     bombs[i] = None
                     beam = None
+                    score.num += 1
         bombs = [bomb for bomb in bombs if bomb is not None]
 
         key_lst = pg.key.get_pressed()
@@ -190,6 +208,7 @@ def main():
             beam.update(screen)   
         for bomb in bombs:
             bomb.update(screen)
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
